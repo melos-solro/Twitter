fa9fa7c8f6bf1e761ac139a6a3f23e5366ea3b99
melos-solro <Melos@meloss-mbp-2.home>
2016-02-23 00:02:31 -0500

    Initial work commit. Monday night.

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,49 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Melos on 2/22/16.
+//  Copyright © 2016 codepath. All rights reserved.
+//
+
+import UIKit
+import BDBOAuth1Manager
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func onLoginButton(sender: AnyObject) {
+        let twitterClient = BDBOAuth1SessionManager(baseURL: NSURL(string: "https://api.twitter.com"), consumerKey: "gxecWzOxuT85NmRVRKCAhqJdX", consumerSecret: "CAYWZliiPke2mWFYzlTdojdxw62R52W6eATxWxJSVzSvJ8Jk8r")
+        twitterClient.deauthorize()
+        twitterClient.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "twitterdemo://oauth"), scope: nil, success: { (requestToken: BDBOAuth1Credential!) -> Void in
+            print("I got a token!")
+            let url = NSURL(string: "https://api.twitter.com/oauth/authorize")!
+            UIApplication.sharedApplication().openURL(url)
+            
+        }, failure: { (error: NSError!) -> Void in
+                print("error: \(error.localizedDescription)")
+        })
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
